6d7baedc0769b8890d463b853aecb75263e995c7
Brittany Miller <britmill@gmail.com>
2018-09-01 16:06:29 -0600

    touch and mkdir func add

--- terminal-commands.js
+++ terminal-commands.js
@@ -9,3 +9,21 @@ module.exports.ls = () => {
     console.log(filesToString);
     });
 };
+
+module.exports.touch = () => {
+    fs.writeFile('message.txt', 'Wow this worked!', 'utf8', (err) => {
+        if (err) throw (err);
+
+        console.log('great success');
+    });
+};
+
+module.exports.mkdir = () => {
+    fs.mkdir('./', function(err) {
+        if (err) {
+            return console.log('Failed to create directory', err);
+        } else {
+            console.log('Directory created');
+        }
+    });
+};
